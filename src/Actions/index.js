d498267f54b75b2a2121ef0feee69bd5443bc7c8
ManikandanRamSaro <mani95ram@outlook.com>
2021-01-21 15:07:08 +0530

    app completed - 21-1-21

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -6,9 +6,10 @@ import _ from 'lodash';
 export const fetchAllPostByOne = () => async (dispatch,getState) =>{
     await dispatch(fetchPost()); 
 
-    const uniUser= _.uniq(_.map(getState().ptext,'userId'));
-   
-    uniUser.forEach(id => dispatch(fetchUser(id)));
+    // const uniUser= _.uniq(_.map(getState().ptext,'userId'));
+    // uniUser.forEach(id => dispatch(fetchUser(id)));  //-> these 2 lines are avoided using below line 
+
+    _.chain(getState().ptext).map('userId').uniq().forEach(id=> dispatch(fetchUser(id))).value();
 
 }
 
